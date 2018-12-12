14738f7cf54507f60b9f65f74b631ea6bf2e60c0
JackShew <jack@shew.net>
2018-12-12 11:59:55 -0800

    Almost final

--- scripts/main.js
+++ scripts/main.js
@@ -2,29 +2,32 @@ $(document).ready(function(){
     $(".button-holder").click(function(){
 		$(".slide2").removeClass("none");
 		$(".slide2").addClass("display");
-   		$(".slide2").fadeIn("slow");
+   		$(".slide2").fadeIn("fast");
 	});
 	$(".exit").click(function(){
 		$(".slide2").addClass("none");
-   		$(".slide2").fadeOut("slow");
+   		$(".slide2").fadeOut("fast");
 	});
 	$(".video-button-holder").click(function(){
 		$(".slide3").removeClass("none");
 		$(".slide3").addClass("display");
-   		$(".slide3").fadeIn("slow");
+   		$(".slide3").fadeIn("fast");
 	});
 	$(".exit-video").click(function(){
 		$(".slide3").addClass("none");
-   		$(".slide3").fadeOut("slow");
+   		$(".slide3").fadeOut("fast");
 	});
 	$(".logo-button-holder").click(function(){
 		$(".slide4").removeClass("none");
 		$(".slide4").addClass("display");
-   		$(".slide4").fadeIn("slow");
+		   $(".slide4").fadeIn("fast");
+		$(".exit-logo").addClass("display");
+		$(".exit-logo").fadeIn("fast");
 	});
 	$(".exit-logo").click(function(){
 		$(".slide4").addClass("none");
-   		$(".slide4").fadeOut("slow");
+   		$(".slide4").fadeOut("fast");
 	});
+
 });
 
